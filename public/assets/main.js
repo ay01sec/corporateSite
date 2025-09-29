4a939dbc5e133ec744cfff331dec338399da482b
yukiya <ay01sec@gmail.com>
2025-09-30 01:16:22 +0900

    1.7

--- public/assets/main.js
+++ public/assets/main.js
@@ -24,10 +24,10 @@ function renderHeader(){
         <a href="/index.html">ホーム</a>
         <a href="/services.html">サービス</a>
         <a href="/works.html">実績</a>
-        <a href="/pricing.html">料金</a>
-        <a href="/faq.html">FAQ</a>
+        <a href="/pricing.html">料金/FAQ</a>
         <a href="/company.html">会社情報</a>
-        <a href="/contact.html" class="btn">お問い合わせ</a>
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLSfd5kkdbHRSh6pSzi-ccTw8eQaxFCvL4S5npUwi79jR6Eznkw/viewform?usp=header" class="btn">無料ヒアリングを依頼</a>
+お問い合わせ</a>
       </div>
     </nav>
   </header>`;
